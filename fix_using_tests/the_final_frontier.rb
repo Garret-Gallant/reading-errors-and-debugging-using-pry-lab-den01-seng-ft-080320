4eaf7c30dc5c6dac1114599c3990ccb55edefb54
Garret Gallant <gallanttechnological@gmail.com>
2020-07-21 22:53:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -6,12 +6,11 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  state_log
-  generate_st
+  puts state_log(date)
+  generate_star_date = date
 end
